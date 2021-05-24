9934b79dc97197435765c0fcf0448c01ddc35278
Yunkon (Alvin) Kim <hermitkim1@gmail.com>
2021-05-24 16:01:01 +0900

    Commit source code initially

---
+++ go.mod
@@ -0,0 +1,9 @@
+module hermitkim1/rtt-test-in-a-cluster
+
+go 1.15
+
+require (
+	github.com/go-ping/ping v0.0.0-20210506233800-ff8be3320020
+	golang.org/x/net v0.0.0-20210521195947-fe42d452be8f // indirect
+	golang.org/x/sys v0.0.0-20210521203332-0cec03c779c1 // indirect
+)
---
+++ rtt-test-in-a-cluster.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"github.com/go-ping/ping"
+)
+
+func main() {
+	pinger, err := ping.NewPinger("129.254.75.33")
+	if err != nil {
+		panic(err)
+	}
+	pinger.OnRecv = func(pkt *ping.Packet) {
+		fmt.Printf("%d bytes from %s: icmp_seq=%d time=%v\n",
+			pkt.Nbytes, pkt.IPAddr, pkt.Seq, pkt.Rtt)
+	}
+
+	pinger.Count = 10
+	pinger.Run() // blocks until finished
+
+	stats := pinger.Statistics() // get send/receive/rtt stats
+	fmt.Printf("round-trip min/avg/max/stddev = %v/%v/%v/%v\n",
+		stats.MinRtt, stats.AvgRtt, stats.MaxRtt, stats.StdDevRtt)
+}
